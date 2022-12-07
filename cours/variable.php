92403b935478f566a38bee40e8997549c921f2da
lim-91 <limery.s1997@gmail.com>
2022-12-07 15:14:19 +0100

    variable

---
+++ cours/variable.php
@@ -0,0 +1,29 @@
+<?php
+// Exercice 1
+ $prenom = "sherley";
+ echo $prenom;
+ $nom = "limery";
+ echo $nom;
+ $age = 25;
+echo $age;
+
+// Exercice 2
+ $km = 1;
+ var_dump($km);
+ $km = 3;
+ var_dump($km);
+ $km = 125;
+ var_dump($km);
+
+// Exercice 3
+ $a = "bonjour";
+ // String
+ $a = 24;
+ // Int
+ $a = 25.5;
+ // Float
+ $a = true / false;
+ // Boolean
+
+
+?>
